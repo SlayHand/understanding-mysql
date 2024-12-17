1e7b6a774abf428b9c5132e9fc0aaaa3736e8c39
fredy <fredy-andri.mottus@voco.ee>
2024-12-17 15:08:53 +0200

    first commit

---
+++ demo_create_table.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2')
+
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "dbuser",
+  password: "qwerty",
+  database: "mydb"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    var sql = "ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY";
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("Table created");
+    });
+  });
